a8e209f8f98b883a23425fb94478f38197f23e0c
smadha <msharan@usc.edu>
2016-11-01 16:22:48 -0700

    Load data and divide it into test and training data

---
+++ as2/data_utils.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+def normalize(X_tr, X_te):
+    ''' Normalize training and test data features
+    Args:
+        X_tr: Unnormalized training features
+        X_te: Unnormalized test features
+    Output:
+        X_tr: Normalized training features
+        X_te: Normalized test features
+    '''
+    X_mu = np.mean(X_tr, axis=0)
+    X_tr = X_tr - X_mu
+    X_sig = np.std(X_tr, axis=0)
+    X_tr = X_tr/X_sig
+    X_te = (X_te - X_mu)/X_sig
+    return X_tr, X_te
+
+def loaddata(filename, col_selected=None, test_data=0.2):
+    ''' Load and preprocess dataset
+    Args:
+        filename: path to data file multimodal.csv
+        col_selected: features selected
+        test_data: percentage of total data to be kept for testing
+    Output:
+        X_tr: normalised training features
+        y_tr: training labels
+        X_te: normalised test features
+        y_te: test labels
+    '''
+    
+    # load all data from filename 
+    with open(filename, 'r') as f:
+        header_list = f.readline()
+    f.close()
+    header_list = header_list.split(",")
+    
+    # By default exclude "id column" and put all other columns
+    if col_selected == None:
+        col_selected = range(1, len(header_list))
+    else:
+        col_selected = set(col_selected)
+        col_selected.add(len(header_list)-1)
+        
+    # Load data into X excluding headers 
+    X = np.genfromtxt(filename, delimiter=",",usecols=col_selected, skip_header=1)
+    
+    # Shuffle data to avoid any local maxima
+    np.random.shuffle(X)
+    
+    #last column is label, fork it out from X into y
+    y = X[:,len(X[0])-1]
+    X = np.delete(X, [len(X[0])-1], axis=1)
+    
+    n_tr = int( (1 - test_data) * len(y) )
+    
+    
+    X_tr = X[0:n_tr]
+    y_tr = y[0:n_tr]
+
+    X_te = X[n_tr:]
+    y_te = y[n_tr:]
+    
+    X_tr,X_te = normalize(X_tr, X_te)
+    return X_tr,y_tr,X_te,y_te
+
+
+if __name__ == '__main__':
+    '''
+    Example how to call loaddata
+    '''
+    print loaddata("test.csv")
+    print loaddata("test.csv", col_selected=[1])
+    print loaddata("test.csv", col_selected=[1,2])
+    
+    
