8d13cf81713f802ff5ed839886ecd194e4fe10cf
smadha <msharan@usc.edu>
2016-11-01 18:39:40 -0700

    updated function

--- as2/svm_utils.py
+++ as2/svm_utils.py
@@ -15,37 +15,39 @@ CV_FOLDS = 2
 # Number of parallel jobs
 parallel = 1
 
-X_tr,y_tr,X_te,y_te = loaddata("test.csv")
 
-svr = SVC()
-
-parameters = [{ 'kernel':['poly'], 'C':C_range, 'degree':deg_range },
-              { 'kernel':['rbf'], 'C':C_range, 'gamma':gamma_ramge}]
-
-clf = GridSearchCV(svr, parameters, cv=CV_FOLDS, n_jobs = parallel, verbose=False)
-
-clf.fit(X_tr,y_tr)
-
-print("Best parameters set found on development set:")
-print()
-print(clf.best_params_)
-print()
-print("Grid scores on development set:")
-print()
-means = clf.cv_results_['mean_test_score']
-stds = clf.cv_results_['std_test_score']
-for mean, std, params in zip(means, stds, clf.cv_results_['params']):
-    print("%0.3f (+/-%0.03f) for %r"
-          % (mean, std * 2, params))
-print()
-
-print("Detailed classification report:")
-print()
-print("The model is trained on the full development set.")
-print("The scores are computed on the full evaluation set.")
-print()
-y_true, y_pred = y_te, clf.predict(X_te)
-print(classification_report(y_true, y_pred))
-print()
+def report_svm(X_tr,y_tr,X_te,y_te, print_CV_report = False):
+    svr = SVC()
+    
+    parameters = [{ 'kernel':['poly'], 'C':C_range, 'degree':deg_range },
+                  { 'kernel':['rbf'], 'C':C_range, 'gamma':gamma_ramge}]
+    
+    clf = GridSearchCV(svr, parameters, cv=CV_FOLDS, n_jobs = parallel, verbose=False)
+    
+    clf.fit(X_tr,y_tr)
+    
+    print("Best parameters set found on development set:")
+    print()
+    print(clf.best_params_)
+    if print_CV_report:
+        print()
+        print("Grid scores on development set:")
+        print()
+        means = clf.cv_results_['mean_test_score']
+        stds = clf.cv_results_['std_test_score']
+        print("mean \tstd \t\tparams")
+        for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+            print("%0.3f (+/-%0.03f) for %r"
+                  % (mean, std * 2, params))
+    
+    print()
+    
+    print("Detailed classification report on test data:")
+    y_true, y_pred = y_te, clf.predict(X_te)
+    print(classification_report(y_true, y_pred))
+    print()
         
 
+if __name__ == '__main__':
+    X_tr,y_tr,X_te,y_te = loaddata("test.csv")
+    report_svm(X_tr,y_tr,X_te,y_te)
