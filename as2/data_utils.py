4c5b8d9cd9d7e1dd2c66f086bc5f6fcda5799c45
smadha <msharan@usc.edu>
2016-11-03 23:11:02 -0700

    returning folds of training data

--- as2/data_utils.py
+++ as2/data_utils.py
@@ -36,6 +36,7 @@ def loaddata(filename, col_selected=None, test_size=0.25, split=4):
         filename: path to data file multimodal.csv
         col_selected: features selected
         test_size: percentage of total data to be kept for testing
+        split = split_size 
     Output:
         X_tr: normalised training features
         y_tr: training labels
@@ -51,20 +52,54 @@ def loaddata(filename, col_selected=None, test_size=0.25, split=4):
     
     # By default exclude "id column" and put all other columns
     if col_selected == None:
-        col_selected = range(1, len(header_list))
+        col_selected = range(0, len(header_list))
     else:
         col_selected = set(col_selected)
         col_selected.add(len(header_list)-1)
+        col_selected.add(0)
         
     # Load data into X excluding headers 
     X = np.genfromtxt(filename, delimiter=",",usecols=col_selected, skip_header=1)
     
     # Shuffle data to avoid any local maxima
     #np.random.shuffle(X)
-
+    
+    # testing data, all folds of training data
     test_split,train_split = getSplitIndex(X, test_size * len(X))
     train_sample_size = len(train_split)
-    print len(test_split),len(train_split)
+    
+    #Normalize data
+    #last column is label, fork it out from X into y
+    # id is column 0
+    y_te = test_split[:,len(X[0])-1]
+    X_te = test_split[:,1:len(X[0])-1]
+    
+    print "Total length of loaded data", len(X[0])
+    print "Length Test features -",  len(X_te[0])
+    
+    #last column is label, fork it out from X into y
+    # id is column 0
+    id_tr = train_split[:,0]
+    y_tr = train_split[:,len(X[0])-1]
+    X_tr = train_split[:,1:len(X[0])-1]
+    
+    print "Length Training features -",  len(X_tr[0])
+    
+    # Normalize training and test features 
+    X_tr,X_te = normalize(X_tr, X_te)
+    
+    # append normalized X_tr with y_tr to pass it for CV sets
+    z = np.zeros((len(y_tr),1))
+    z[:,0]=y_tr
+    train_split=np.append(X_tr, z, axis=1)
+    # append id in the start
+    z = np.zeros((len(id_tr),1))
+    z[:,0]=id_tr
+    train_split = np.append(z,train_split, axis=1)
+    
+    print "Length cols in train_split -",  len(train_split[0])
+    
+    # all training splits
     train_samples = []
     split_size = (train_sample_size/split)
     i = 1
@@ -74,16 +109,19 @@ def loaddata(filename, col_selected=None, test_size=0.25, split=4):
         train_samples.append(train_subsplit)
         i=i+1
     train_samples.append(train_split)
-    #last column is label, fork it out from X into y
-    #y_te = test_split[:,len(X[0])-1]
-    #X_te = test_split[:,0:len(X[0])-2]
-    #X = np.delete(X, [len(X[0])-1], axis=1)
     
-    #X_tr,X_te,y_tr,y_te = train_test_split(X, y, test_size=test_size, random_state=42)
-    
-    #X_tr,X_te = normalize(X_tr, X_te)
-    
-    return test_split,train_samples
+    X_tr,y_tr = [],[]
+    for fold in train_samples:
+        last_col = len(fold[0])
+        X_fold = fold[:,1:last_col-1]
+        y_fold = fold[:,last_col-1]
+        X_tr.append(X_fold)
+        y_tr.append(y_fold)
+        
+        print "Features in X_fold", len(X_fold[0]), len(y_fold), len(X_fold)
+        
+    print len(X_tr), len(y_tr), len(X_te), len(y_te)
+    return X_tr,y_tr,X_te,y_te    
 
 
 if __name__ == '__main__':
